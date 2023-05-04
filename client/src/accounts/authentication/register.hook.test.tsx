c26db52362884477ccd030fcab03b2c736a01a09
stevefaeembra <stevendkay@gmail.com>
2023-05-04 12:22:17 +0100

    fix(frontend): Fix tests to use toBe/strictEqual
    should be toBe() for basic types, toStrictEqual() for
    object comparisons. Also, fix for ESLint.

--- client/src/accounts/authentication/register.hook.test.tsx
+++ client/src/accounts/authentication/register.hook.test.tsx
@@ -1,18 +1,22 @@
+/*eslint import/namespace: ["off"]*/
 import { describe, expect, it } from 'vitest';
+
 import { HTTP_BAD_REQUEST, HTTP_OK } from '~/api/api.constants';
 import { rest, server } from '~/mocks/server';
 import { act, renderHook, waitFor } from '~/test/utils';
+
 import { RegistrationFormType, useRegister } from './register.hook';
 
 const ENDPOINT = '*/api/accounts/register/';
 
 interface Result {
-  id: Number;
+  id: number;
   error?: { message: string };
   mutate: (form: Record<string, unknown>) => void;
   isError: boolean;
   isSuccess: boolean;
   data?: Record<string, unknown>;
+  status: string;
 }
 
 describe('useRegisterHook', () => {
@@ -35,7 +39,7 @@ describe('useRegisterHook', () => {
     act(() => result.current.mutate(registerForm));
 
     await waitFor(() => expect(result.current.isError).toBe(true));
-    expect(result.current.error).toEqual({ message: expectedMessage });
+    expect(result.current.error).toStrictEqual({ message: expectedMessage });
   });
 
   it('should throw an error if email is malformed', async () => {
@@ -62,11 +66,10 @@ describe('useRegisterHook', () => {
     await waitFor(() => {
       expect(result.current.isError).toBe(true);
     });
-    expect(result.current.error).toEqual({ message: expectedMessage });
+    expect(result.current.error).toStrictEqual({ message: expectedMessage });
   });
 
   it('should throw an error if email has already been registered', async () => {
-    // attempt to reuse existing email address
     const expectedMessage = 'Please re-authenticate';
     server.use(
       rest.post(ENDPOINT, (req, res, ctx) => res(ctx.status(HTTP_BAD_REQUEST), ctx.json({ message: expectedMessage }))),
@@ -86,7 +89,7 @@ describe('useRegisterHook', () => {
     });
 
     await waitFor(() => expect(result.current.isError).toBe(true));
-    expect(result.current.error).toEqual({ message: expectedMessage });
+    expect(result.current.error).toStrictEqual({ message: expectedMessage });
   });
 
   it('should show success code if registration was successful', async () => {
@@ -109,7 +112,7 @@ describe('useRegisterHook', () => {
     await waitFor(() => {
       expect(result.current.isSuccess).toBe(true);
     });
-
-    expect(result.current.status).toEqual('success');
+    expect(result.current.status).toBe('success');
+    expect(result.current.data).toBe(1); // returns an id
   });
 });
